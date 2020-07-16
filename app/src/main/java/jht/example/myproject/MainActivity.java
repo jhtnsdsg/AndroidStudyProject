9013e0c85c6389234484a4a57ef4f52f6e77cf52
“JHT <jhtnsdsg@163.com>
2020-07-16 23:02:32 +0800

    null

--- app/src/main/java/jht/example/myproject/MainActivity.java
+++ app/src/main/java/jht/example/myproject/MainActivity.java
@@ -3,14 +3,16 @@ package jht.example.myproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG="AppCompatActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d(TAG,"onCreat execute");
     }
 }

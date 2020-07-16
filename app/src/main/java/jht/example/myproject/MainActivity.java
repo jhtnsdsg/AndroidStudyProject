5b185d865fb68344a2fb853b880c37244465e7bd
“JHT <jhtnsdsg@163.com>
2020-07-16 21:08:55 +0800

    Initial commit

---
+++ app/src/main/java/jht/example/myproject/MainActivity.java
@@ -0,0 +1,16 @@
+package jht.example.myproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
